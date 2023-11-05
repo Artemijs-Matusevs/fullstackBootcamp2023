420e3c29f89240315204d85e43587b534a835600
Artemijs Matusev <matu6ev@gmail.com>
2023-11-05 16:18:53 +0000

    Exercise 8.3 Travel Tracker - Completed Part 1

--- Exercises/007 PostgreSQL/8.3 Travel Tracker/index.js
+++ Exercises/007 PostgreSQL/8.3 Travel Tracker/index.js
@@ -1,5 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "world",
+  password: "",
+  port: 5432,
+})
 
 const app = express();
 const port = 3000;
@@ -7,8 +16,24 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let countryCodes = [];
+
+//Connect to db and query for all the country codes
+db.connect();
+
+db.query("SELECT country_code FROM visited_countries", (err, res) => {
+  if (err) {
+    console.error("Error executing query", err.stack);
+  } else {
+    countryCodes = res.rows.map(obj => obj.country_code);
+    //console.log(countryCodes);
+  }
+  db.end();
+})
+
 app.get("/", async (req, res) => {
   //Write your code here.
+  res.render("index.ejs", {countries: countryCodes, total: countryCodes.length});
 });
 
 app.listen(port, () => {
