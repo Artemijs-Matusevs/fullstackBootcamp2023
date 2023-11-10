8ffb54d88042611b8a87ff33f051918b8db507e9
Artemijs Matusev <matu6ev@gmail.com>
2023-11-10 12:04:14 +0000

    Finished exercise 8.6 Permalist Project

--- Exercises/007 PostgreSQL/8.6 Permalist Project/index.js
+++ Exercises/007 PostgreSQL/8.6 Permalist Project/index.js
@@ -1,33 +1,103 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
 
+//MIDLEWARE AND STATIC FILES
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [
+//Config to connect to the database
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "",
+  port: 5432
+});
+db.connect()
+
+
+/*let items = [
   { id: 1, title: "Buy milk" },
   { id: 2, title: "Finish homework" },
-];
+];*/
 
-app.get("/", (req, res) => {
+
+// ----------- FUNCTIONS -------------
+
+//Function to return current list
+async function checkList() {
+  const result = await db.query("SELECT * FROM list");
+
+  //console.log(result.rows);
+  return result.rows;
+}
+
+//Function to add new item to list table
+async function addItem(item) {
+  await db.query(`
+    INSERT INTO list (title)
+    VALUES ($1)`,
+    [item]);
+}
+
+//Function to remove existing item from list table
+async function deleteItem(itemID) {
+  await db.query(`
+    DELETE FROM list
+    WHERE id = $1`,
+    [itemID]);
+}
+
+//Function to edit existing item by ID 
+async function editItem(itemID, item) {
+  await db.query(`
+    UPDATE list
+    SET title = $1
+    WHERE id = $2`,
+    [item, itemID]);
+}
+
+
+
+
+// ----------- END POINTS -------------
+
+app.get("/", async (req, res) => {
   res.render("index.ejs", {
     listTitle: "Today",
-    listItems: items,
+    listItems: await checkList(),
   });
 });
 
 app.post("/add", (req, res) => {
   const item = req.body.newItem;
-  items.push({ title: item });
+  //console.log(item);
+
+  addItem(item);
   res.redirect("/");
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", (req, res) => {
+  //console.log(req.body);
+  const newTitle = req.body.updatedItemTitle;
+  const itemId = req.body.updatedItemId;
 
-app.post("/delete", (req, res) => {});
+  editItem(itemId, newTitle);
+  res.redirect("/");
+
+});
+
+app.post("/delete", (req, res) => {
+  const item = req.body.deleteItemId;
+  //console.log(req.body);
+
+  deleteItem(item);
+  res.redirect("/");
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
