d43ad2871ff52bb89a81f75912465b0505eb2ace
Artemijs Matusev <matu6ev@gmail.com>
2024-01-23 20:21:21 +0000

    Set up the login endpoint on task 008

--- Exercises/008 Authentication and Security/9.1+Authentication+Lv.1/index.js
+++ Exercises/008 Authentication and Security/9.1+Authentication+Lv.1/index.js
@@ -10,7 +10,7 @@ const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "Users",
-  password: "10151015",
+  password: "",
   port: 5432,
 });
 db.connect();
@@ -31,22 +31,36 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
+
+
 app.post("/register", async (req, res) => {
 
   //Get submited details from register form
   const email = req.body.username;
   const password = req.body.password;
 
+  try{
+    //Check if user already exists
+    const checkResult = await db.query("SELECT * FROM users WHERE email = $1", 
+    [email]);
 
-  //insert data into db
-  const result = await db.query(
-    "INSERT INTO users (email, password) VALUES ($1, $2)",
-    [email, password]
-  );
-  console.log(result);
-  res.render("secrets.ejs");//Render secrets
+    //insert data into db
+    if (checkResult.rows.length > 0){
+      res.send("Email already exists")
+    }else{
+      const result = await db.query(
+        "INSERT INTO users (email, password) VALUES ($1, $2)",
+        [email, password]
+      );
+      console.log(result);
+      res.render("secrets.ejs");//Render secrets
+    }
+  }catch(err){
+    console.log(err);
+  }
 
 });
+
 
 app.post("/login", async (req, res) => {
 
@@ -54,6 +68,27 @@ app.post("/login", async (req, res) => {
   const email = req.body.username;
   const password = req.body.password;
 
+  //See if user exists
+  try{
+    //Get user details
+    const storedResults = await db.query("SELECT * FROM users WHERE email = $1", 
+    [email]);
+    //console.log(storedDetails.rows[0].email);
+    const storedPassword = storedResults.rows[0].password;
+
+    //compare passwords
+    if(storedPassword == password){
+      //console.log("Passwords match");
+      res.render("secrets.ejs");//Render secrets
+    }
+    else{
+      //console.log("Passwords don't match");
+      res.send("Wrong password")
+    }
+
+  }catch(err){
+    console.log(err);
+  }
 
 });
 
